3d03bdd473de72d25c5920249b542fa47e1bd74b
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-31 17:28:36 +0100

    Update frontend layout structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Product from "./pages/Product";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
