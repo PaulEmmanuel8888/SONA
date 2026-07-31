46695eacbb5c38e3c967df3dcefef1ecca325fdc
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-31 22:59:13 +0100

    Update Mobile Navbar menu with react-router Link

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -2,18 +2,48 @@ import SONALogo from "../../assets/images/SONA.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBarsStaggered, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="flex flex-row justify-between w-full shadow items-center px-6 py-4">
-      <FontAwesomeIcon
-        onClick={() => setIsMenuOpen((prev) => !prev)}
-        icon={isMenuOpen ? faXmark : faBarsStaggered}
-      />
-      <img src={SONALogo} alt="SONA Logo" className="h-10 w-auto mr-4" />
-      <span>Cart Icon</span>
-    </div>
+    <>
+      {
+        /* Mobile Navbar */
+        <div className="flex flex-row justify-between w-full shadow items-center px-6 py-4">
+          <Link to={`/`}>
+            <img src={SONALogo} alt="SONA Logo" className="h-10 w-auto mr-4" />
+          </Link>
+          <span>Cart Icon</span>
+          <FontAwesomeIcon
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+            icon={isMenuOpen ? faXmark : faBarsStaggered}
+          />
+        </div>
+      }
+
+      {
+        // Mobile Menu
+        isMenuOpen && (
+          <div className="flex flex-col items-center justify-center ">
+            <ul className="flex flex-col m-2 p-5 text-lg border [&>li]:m-4">
+              <li>
+                <Link to={`/#features`}>Features</Link>
+              </li>
+              <li>
+                <Link to={`/#gallery`}>Gallery</Link>
+              </li>
+              <li>
+                <Link to={`/#specs`}>Specs</Link>
+              </li>
+              <li>
+                <Link to={`/#reviews`}>Reviews</Link>
+              </li>
+            </ul>
+          </div>
+        )
+      }
+    </>
   );
 };
 
