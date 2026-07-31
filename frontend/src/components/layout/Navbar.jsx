8228f93b4dc16a19377645c5ac0e30c2b64c4fa9
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-31 17:47:01 +0100

    Add SONA logo and update Navbar component

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,5 +1,24 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img
+            src="../../assets/images/SONA_logo.png"
+            alt="SONA logo"
+            className="w-10 h-10"
+          />
+        </div>
+        <div className="flex items-center">
+          <ul className="flex items-center">
+            <li className="mr-4">
+              <a href="/">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
