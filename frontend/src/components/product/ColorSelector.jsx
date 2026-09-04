90e432a669fe751e78dd1600750ffdedafe14e45
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-09-04 17:10:54 +0100

    Add state for colors in Product component

--- frontend/src/components/product/ColorSelector.jsx
+++ frontend/src/components/product/ColorSelector.jsx
@@ -1,20 +1,34 @@
-const ColorSelector = () => {
+const ColorSelector = ({ colors, selectedColor, setSelectedColor }) => {
   return (
     <div className="flex flex-col items-center lg:items-start gap-6">
       <div>
         <p className="text-sm text-gray-500 uppercase tracking-wider">Color</p>
 
-        <h2 className="text-2xl font-medium mt-2">White</h2>
+        <h2 className="text-2xl font-medium mt-2">{selectedColor.name}</h2>
       </div>
 
       <div className="flex gap-4">
-        <button className="cursor-pointer hover:scale-110 w-12 h-12 rounded-full border-none bg-white ring-2 ring-blue-300 ring-offset-2" />
-
-        <button className="cursor-pointer hover:scale-110 w-12 h-12 rounded-full bg-black border border-black/10" />
-
-        <button className="cursor-pointer hover:scale-110 w-12 h-12 rounded-full bg-[#D9D9D6] border border-black/20" />
-
-        <button className="cursor-pointer hover:scale-110 w-12 h-12 rounded-full bg-[#FFC0CB] border border-black/10" />
+        {colors.map((color) => (
+          <button
+            key={color.name}
+            onClick={() => setSelectedColor(color)}
+            aria-label={`Select ${color.name}`}
+            className={`
+              cursor-pointer
+              hover:scale-110
+              transition-transform
+              w-12 h-12
+              rounded-full
+              border
+              ${
+                selectedColor.name === color.name
+                  ? "ring-2 ring-blue-300 ring-offset-2"
+                  : "border-black/20"
+              }
+            `}
+            style={{ backgroundColor: color.value }}
+          />
+        ))}
       </div>
     </div>
   );
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -5,8 +5,29 @@ import Container from "../components/layout/Container";
 import ProductImage from "../components/product/ProductImage";
 
 import ColorSelector from "../components/product/ColorSelector";
+import { useState } from "react";
+
+const colors = [
+  {
+    name: "White",
+    value: "#FFFFFF",
+  },
+  {
+    name: "Black",
+    value: "#000000",
+  },
+  {
+    name: "Silver",
+    value: "#D9D9D6",
+  },
+  {
+    name: "Pink",
+    value: "#FFC0CB",
+  },
+];
 
 const Product = () => {
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
   return (
     <>
       <Container>
@@ -15,9 +36,13 @@ const Product = () => {
 
       <Container className="mt-[10vh]">
         <div className="grid grid-cols-1 lg:grid-cols-[1.5fr_1fr] gap-10 lg:gap-16 items-center">
-          <ProductImage />
+          <ProductImage color={selectedColor} />
 
-          <ColorSelector />
+          <ColorSelector
+            colors={colors}
+            selectedColor={selectedColor}
+            setSelectedColor={setSelectedColor}
+          />
         </div>
       </Container>
     </>
