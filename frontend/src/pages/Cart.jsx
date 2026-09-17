defa56b3649c3a726c0db2628e506e2dd919c6e9
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-09-17 15:12:27 +0100

    Set up Cart page to test cart item logic

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,25 @@
+import Container from "../components/layout/Container";
+import { useCart } from "../context/cartContext.jsx";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems } = useCart();
+
+  return (
+    <section id="cart">
+      <Container className="mt-[10vh]">
+        <h1 className="text-4xl font-bold">Your Cart</h1>
+
+        <div className="mt-10">
+          {cartItems.map((item) => (
+            <div key={item.color.name}>
+              <p>{item.color.name}</p>
+              <p>Quantity: {item.quantity}</p>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default Cart;
