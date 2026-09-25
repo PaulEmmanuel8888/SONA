b1206ccf4b7b803857b602e8de596c5fb5475098
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-09-25 17:18:38 +0100

    Create ScrollToTop component to automatically scroll to the top on every new page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/cartContext.jsx";
+import ScrollToTop from "./components/layout/ScrollToTop.jsx";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
+
       <CartProvider>
         <App />
       </CartProvider>
